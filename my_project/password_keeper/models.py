8d167beeea44c2cdf2ed24fd5442008334a3f3f3
Prakash R <prakashramanukolanu1998@gmail.com>
2023-06-27 19:53:38 +0530

    Create AppPassword model

--- my_project/password_keeper/models.py
+++ my_project/password_keeper/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class AppPassword(models.Model):
+    app_name = models.CharField(max_length=100)
+    password = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
