e37a09a674f10563871d49edef597bc811091a24
Prakash R <prakashramanukolanu1998@gmail.com>
2023-07-03 10:51:21 +0530

    Add user_id to model.

--- my_project/password_keeper/models.py
+++ my_project/password_keeper/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class AppPassword(models.Model):
@@ -6,3 +9,4 @@ class AppPassword(models.Model):
     password = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
